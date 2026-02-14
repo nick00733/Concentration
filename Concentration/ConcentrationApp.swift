046ea1f39f3241f1b3cd0e6f07288fef25498a98
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-14 15:55:58 -0500

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Nick Nedjat on 14.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
